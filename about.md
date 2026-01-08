eb2020225ea18c732ce061bc24805c478a4b8e3f
Shroomagi <140358230+Shroomagi@users.noreply.github.com>
2026-01-07 19:36:18 -0500

    OMG NEW UPDATE REEEEEEE
    GG BOI

--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About
-permalink: about
----
-
-<p style="text-align: center;">Coming Soon</p>
---
+++ script.js
@@ -0,0 +1,29 @@
+let audioStarted = false;
+
+function startReversedAudio() {
+  if (audioStarted) return;
+  audioStarted = true;
+
+  const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+  fetch("intro.mp3")
+    .then(r => r.arrayBuffer())
+    .then(b => ctx.decodeAudioData(b))
+    .then(buffer => {
+      for (let c = 0; c < buffer.numberOfChannels; c++) {
+        Array.prototype.reverse.call(buffer.getChannelData(c));
+      }
+
+      const src = ctx.createBufferSource();
+      src.buffer = buffer;
+      src.loop = true;
+      src.connect(ctx.destination);
+      src.start();
+    });
+
+  window.removeEventListener("click", startReversedAudio);
+  window.removeEventListener("keydown", startReversedAudio);
+}
+
+window.addEventListener("click", startReversedAudio);
+window.addEventListener("keydown", startReversedAudio);
